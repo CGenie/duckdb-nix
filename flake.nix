ed609cf66e2d957a80365a759846d86b4f819837
Przemysław Kaminski <pk@intrepidus.pl>
2025-04-10 06:24:31 +0200

    duckdb 1.2.2

--- flake.nix
+++ flake.nix
@@ -25,6 +25,9 @@
         main = pkgs.callPackage ./packages/main.nix {
           stdenv = pkgs.libcxxStdenv;
         };
+        v1_2_2 = pkgs.callPackage ./packages/v1.2.2.nix {
+          stdenv = pkgs.stdenv;
+        };
         v1_2_1 = pkgs.callPackage ./packages/v1.2.1.nix {
           stdenv = pkgs.stdenv;
         };
@@ -41,7 +44,7 @@
       formatter = pkgs.alejandra;
 
       devShells.default = pkgs.mkShell {
-        buildInputs = [ packages.v1_2_1 ];
+        buildInputs = [ packages.v1_2_2 ];
       };
     });
   in
